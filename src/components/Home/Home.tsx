cdce477fe5c060fe48b4b6364d3023bb7703404e
STkangyh <k22101149@g.seoultech.ac.kr>
2024-09-07 20:49:43 +0900

    Preview: Preview, Emptyview

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -1,3 +1,4 @@
+import Emptyview from './Emptyview';
 import Preview from './Preview';
 
 export default function Home() {
@@ -8,42 +9,67 @@ export default function Home() {
       image: <img src='image1.jpg' alt='Profile 1' />,
       name: '홍길동',
       relationship: '친구',
-      birthdate: '1990-01-01',
-      deathDate: '2020-01-01',
+      birthdate: '1990.01.01',
+      deathDate: '2020.01.01',
     },
     {
       id: 2,
       image: <img src='image2.jpg' alt='Profile 2' />,
       name: '김철수',
       relationship: '동료',
-      birthdate: '1985-05-12',
-      deathDate: '2019-08-20',
+      birthdate: '1985.05.12',
+      deathDate: '2019.08.20',
+    },
+    {
+      id: 3,
+      image: <img src='asdfad' alt='Profile 3' />,
+      name: '엥',
+      relationship: '엥',
+      birthdate: '1232.12.12',
+      deathDate: '1232.13.13',
     },
     // 더 많은 프로필 추가 가능
   ];
 
   return (
-    <div>
-      <div className='profile'>
-        <div className='image'>image 자리</div>
-        <div className='others'>
-          <div className='name'>이름 자리</div>
-          <div className='state'>상메</div>
+    <div className='w-screen min-h-screen relative bg-[#1a1e29]'>
+      <div className='w-96 h-11 left-0 top-0 absolute'>
+        <div className='w-16 h-3 left-[289.67px] top-[17.33px] absolute'>
+          <div className='w-6 h-3 left-[42.33px] top-0 absolute'></div>
         </div>
       </div>
 
-      {/* 여러 명의 Preview 컴포넌트 렌더링 */}
-      <div className='stars'>
-        {profiles.map((profile) => (
-          <Preview
-            key={profile.id}
-            image={profile.image}
-            name={profile.name}
-            relationship={profile.relationship}
-            birthdate={profile.birthdate}
-            deathDate={profile.deathDate}
+      <div className='w-96 h-28 px-4 py-4 left-0 top-[44px] absolute bg-[#1a1e29] border-[#343434] flex-col justify-start items-start gap-2.5 inline-flex'>
+        <div className='justify-start items-center gap-4 inline-flex'>
+          <img
+            className='w-20 h-20 rounded-full'
+            src='https://via.placeholder.com/80x80'
           />
+          <div className='flex-col justify-start items-start gap-3 inline-flex'>
+            <div className="self-stretch text-white text-xl font-normal font-['Source Han Serif K'] leading-tight">
+              박진아{' '}
+            </div>
+            <div className="self-stretch text-white/70 text-base font-light font-['Pretendard'] leading-none">
+              상태메세지
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className='w-100% left-[16px] right-[16px] top-[180px] absolute grid grid-cols-2 gap-2'>
+        {profiles.map((profile) => (
+          <div className=''>
+            <Preview
+              key={profile.id}
+              image={profile.image}
+              name={profile.name}
+              relationship={profile.relationship}
+              birthdate={profile.birthdate}
+              deathDate={profile.deathDate}
+            />
+          </div>
         ))}
+        <Emptyview />
       </div>
     </div>
   );
--- src/components/Home/Preview.tsx
+++ src/components/Home/Preview.tsx
@@ -9,12 +9,25 @@ interface PreviewProps {
 export default function Preview(props: PreviewProps) {
   return (
     <div>
-      <div className='image'>{props.image}</div>
-      <div className='others'>
-        <div className='name'>{props.name}</div>
-        <div className='relationship'>{props.relationship}</div>
-        <div className='date'>
-          {props.birthdate} ~ {props.deathDate}
+      <div className='w-100% h-56 px-5 py-5 bg-gradient-to-b from-[#3a4155] to-[#1f2431] rounded-lg shadow border border-white justify-start items-center gap-2.5 flex'>
+        <div className='w-100% flex-col justify-start items-center gap-4 inline-flex'>
+          <div className='h-40 flex-col justify-start items-center gap-3 flex'>
+            <img
+              className='self-stretch h-24 rounded-full border border-white/60'
+              src={props.image}
+            />
+            <div className='h-11 flex-col justify-start items-center gap-3 flex'>
+              <div className="self-stretch text-white text-xl font-normal font-['Source Han Serif K'] leading-tight">
+                {props.name}
+              </div>
+              <div className="self-stretch text-center text-white/80 text-sm font-light font-['Pretendard'] leading-none">
+                {props.relationship}
+              </div>
+            </div>
+          </div>
+          <div className="text-white/50 text-xs font-light font-['Pretendard'] leading-3">
+            {props.birthdate} - {props.deathDate}
+          </div>
         </div>
       </div>
     </div>
