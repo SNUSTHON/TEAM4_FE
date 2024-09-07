fbe1e9dacfca05406b15eb90b686360bda24f814
STkangyh <k22101149@g.seoultech.ac.kr>
2024-09-07 21:42:35 +0900

    Emptyview: emptyview navigation

--- src/components/Home/Emptyview.tsx
+++ src/components/Home/Emptyview.tsx
@@ -1,6 +1,15 @@
+import { useNavigate } from 'react-router';
+
 export default function Emptyview() {
+  const navigate = useNavigate();
+  const handleClick = () => {
+    navigate('/upload');
+  };
   return (
-    <div className='w-100% h-56 px-5 py-5 bg-gradient-to-b from-[#3a4155] to-[#1f2431] rounded-lg shadow border border-white justify-center items-center gap-2.5 inline-flex'>
+    <div
+      className='w-100% h-56 px-5 py-5 bg-gradient-to-b from-[#3a4155] to-[#1f2431] rounded-lg shadow border border-white justify-center items-center gap-2.5 inline-flex'
+      onClick={handleClick}
+    >
       <div className='w-100% flex-col justify-center items-center gap-4 inline-flex'>
         <div className='w-12 h-12 bg-[#f9e55a] rounded-full shadow flex justify-center items-center'>
           <span className='text-3xl text-[#242424] font-bold mb-2'>+</span>
