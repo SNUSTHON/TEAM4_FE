33ddbc26a9af0185ed7c069fc2d9359246dbdd5a
STkangyh <k22101149@g.seoultech.ac.kr>
2024-09-07 18:25:41 +0900

    Uploads: 화면 추가

---
+++ src/components/Uploads/Uploads.tsx
@@ -0,0 +1,11 @@
+import FileUploadForm from './FileUpload';
+
+export default function Uploads() {
+  return (
+    <div className='bg-bgBlack'>
+      <div>
+        <FileUploadForm />
+      </div>
+    </div>
+  );
+}
