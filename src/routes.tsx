0acfc409a381fdd80230715ecf049e16b3fdf960
STkangyh <k22101149@g.seoultech.ac.kr>
2024-09-07 18:23:27 +0900

    route: page 추가

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,11 +1,15 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/home';
+import Upload from './pages/uploads';
+import Galary from './pages/galary';
 
 export default function routes() {
   return (
     <Router>
       <Routes>
         <Route path='/' element={<Home />} />
+        <Route path='/upload' element={<Upload />} />
+        <Route path='/galary' element={<Galary />} />
       </Routes>
     </Router>
   );
