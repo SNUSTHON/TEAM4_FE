5efee87bc76035ce216ce61db2a9a36b77866f1a
STkangyh <k22101149@g.seoultech.ac.kr>
2024-09-07 18:23:12 +0900

    tailwind: background color customizing

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,11 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'bgBlack': '#1A1E29',
+      }
+    },
   },
   plugins: [],
 }
